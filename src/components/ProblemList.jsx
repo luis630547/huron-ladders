f56996614c0a2c131d4a74d9e5d1e28de2a07e90
luis630547 <luis630547@gmail.com>
2020-08-18 00:48:43 -0500

    add difficulty and topic y ladders && solved responsive tabs

--- src/components/ProblemList.jsx
+++ src/components/ProblemList.jsx
@@ -1,46 +1,96 @@
-import React, { Fragment } from "react";
-import { TableContainer, Paper, Table, TableHead, TableBody,Button, TableRow, TableCell, Link, LinearProgress } from "@material-ui/core";
+import React from 'react';
+import {
+	TableContainer,
+	Paper,
+	Table,
+	TableHead,
+	TableBody,
+	Button,
+	TableRow,
+	TableCell,
+	Link,
+	LinearProgress,
+	Grid,
+	Hidden
+} from '@material-ui/core';
 import DoneIcon from '@material-ui/icons/Done';
 
 const ProblemList = ({ problems, isLoading }) => {
-    
-    return (
-        <Fragment>
-            <TableContainer component={Paper} style={{marginTop: "1rem"}}>
-                {isLoading && <LinearProgress />}
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>Id</TableCell>
-                            <TableCell>Key</TableCell>
-                            <TableCell>Name</TableCell>
-                            <TableCell>Status</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {problems.map((problem, id) => (
-                        <TableRow key={id}>
-                            <TableCell>{id+1}</TableCell>
-                            <TableCell><Link href={`https://codeforces.com/problemset/problem/${problem.contest}/${problem.letter}`}>{problem.contest}-{problem.letter}</Link></TableCell>
-                            <TableCell><Link href={`https://codeforces.com/problemset/problem/${problem.contest}/${problem.letter}`}>{problem.name}</Link></TableCell>
-                            <TableCell>{problem.status ? (<Button
-                                                                variant="contained"
-                                                                color="primary"
-                                                                size="small"
-                                                                rel="noopener"
-                                                                target="_blank"
-                                                                href={`https://codeforces.com/contest/${problem.contest}/submission/${problem.status}`}
-                                                                startIcon={<DoneIcon />}
-                                                            >
-                                                                {problem.status}
-                                                            </Button>) : '-'}</TableCell>
-                        </TableRow>
-                    ))} 
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Fragment>
-    )
-}
+	return (
+		<Grid container>
+			<Grid item xs={12}>
+				<TableContainer component={Paper} style={{ margin: '1rem 0' }}>
+					{isLoading && <LinearProgress />}
+					<Table>
+						<TableHead>
+							<TableRow>
+								<TableCell>Id</TableCell>
+								<Hidden xsDown>
+									<TableCell>Key</TableCell>
+								</Hidden>
+								<TableCell>Name</TableCell>
+								<Hidden xsDown>
+									<TableCell>Difficulty</TableCell>
+								</Hidden>
+								<Hidden xsDown>
+									<TableCell>Topic</TableCell>
+								</Hidden>
+								<TableCell>Status</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{problems.map((problem, id) => (
+								<TableRow key={id}>
+									<TableCell>{id + 1}</TableCell>
+									<Hidden xsDown>
+										<TableCell>
+											<Link
+												href={`https://codeforces.com/problemset/problem/${problem.contest}/${problem.letter}`}
+											>
+												{problem.contest}-{problem.letter}
+											</Link>
+										</TableCell>
+									</Hidden>
+									<TableCell>
+										<Link
+											rel="noopener"
+											target="_blank"
+											href={`https://codeforces.com/problemset/problem/${problem.contest}/${problem.letter}`}
+										>
+											{problem.name}
+										</Link>
+									</TableCell>
+									<Hidden xsDown>
+										<TableCell>easy</TableCell>
+									</Hidden>
+									<Hidden xsDown>
+										<TableCell>sorting, math</TableCell>
+									</Hidden>
+									<TableCell>
+										{problem.status ? (
+											<Button
+												variant="contained"
+												color="primary"
+												size="small"
+												rel="noopener"
+												target="_blank"
+												href={`https://codeforces.com/contest/${problem.contest}/submission/${problem.status}`}
+												startIcon={<DoneIcon />}
+											>
+												{problem.status}
+											</Button>
+										) : (
+											'-'
+										)}
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</Grid>
+		</Grid>
+	);
+};
 
 export default ProblemList;
